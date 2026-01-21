cb912ce1700798bf9124ae704a7b18499a543930
Mohit Parmar <mohitrajput61678@gmail.com>
2026-01-21 23:37:43 +0530

    model hyperparameter tuninig

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from src.exception import CustomException
 
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):    
     try:
@@ -21,14 +22,20 @@ def save_object(file_path, obj):
         logging.error("Error occurred while saving object")
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test,models,param):
     try:
         report = {}
         
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            params = param[list(models.keys())[i]]
             
+            gcv=GridSearchCV(model,params,cv=3)
+            gcv.fit(X_train, y_train)
+            
+            model.set_params(**gcv.best_params_)
             model.fit(X_train, y_train)
+            
             
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
